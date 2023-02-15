9efef88865a3051ec0e173c29809d60a3321150b
jay-amin-tops <jay.amin.tops@gmail.com>
2023-02-15 09:58:18 +0530

    login api

--- ReactJs/project/src/Hooks/CustomHook.jsx
+++ ReactJs/project/src/Hooks/CustomHook.jsx
@@ -5,6 +5,7 @@ const CustomHook = (intVal,initError) => {
     const [inp,setInput] = useState(intVal)
     const [errors,setError] = useState(initError)
     const handleChange = (e)=>{
+        // console.log("called");
         // setInput((inp)=>({[e.target.name]:e.target.value}))
         setInput((inp)=>({...inp,[e.target.name]:e.target.value}))
         console.log("called from Hook"); 
@@ -13,6 +14,13 @@ const CustomHook = (intVal,initError) => {
                 setError({...errors,"usernameError":"User Name is required"})
             } else {
                 setError({...errors,"usernameError":""})
+            } 
+        }else if(e.target.name == "email") {
+            let Pattern = /^[a-zA-Z0-9]+$/
+            if (Pattern.test(e.target.value)) {
+                setError({...errors,"emailError":"User Name is required"})
+            } else {
+                setError({...errors,"emailError":""})
             } 
         }
     }
--- ReactJs/project/src/Login.jsx
+++ ReactJs/project/src/Login.jsx
@@ -2,50 +2,86 @@
 import React from 'react';
 import './login.css';
 import CustomHook from './Hooks/CustomHook'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Login = () => {
-    const {handleChange,inp,errors} = CustomHook({},{usernameError:"",passwordError:""})
+    // const CustHookData = CustomHook({},{usernameError:"",passwordError:""})
+    // CustHookData.handleChange
+    const navigate = useNavigate();
+    const { handleChange, inp, errors } = CustomHook({}, { usernameError: "", passwordError: "" })
+    const LoginForm = async (e) => {
+        e.preventDefault();
+        // console.log("login click", inp);
+        let url = "https://justjayapi.000webhostapp.com/login";
+        let LoginRes = await axios.get(url, {
+            params: {
+                username: inp.username,
+                password: inp.password
+            }
+        }).then(function (response) {
+                // console.log("success response");
+                // console.log(response.data.Data[0].role_id);
+                if (response.data.Code == 1) {
+                    if (response.data.Data[0].role_id == 1) {
+                        navigate("/admindashboard")  
+                    }else{
+                        navigate("/")  
+                    }
+                }else{
+                    console.log("Invalid user");
+                }
+                return response;
+            }).catch(function (error) {
+                console.log("If error");
+                console.log(error);
+            })
+            // .finally(function () {
+            //     // console.log("always executed");
+            //     // always executed
+            // });
+        console.log("login click", LoginRes);
+    }
     return (
 
         <div className="container">
-<div className="form-box">
-    <div className="header-form">
-        <h4 className="text-primary text-center"><i className="fa fa-user-circle" style={{ fontSize: "110px" }}></i></h4>
-        <div className="image">
-        </div>
-    </div>
-    <div className="body-form">
-<form>
-    <div className="input-group mb-3">
-        <div className="input-group-prepend">
-            <span className="input-group-text"><i className="fa fa-user"></i></span>
-        </div>
-        <input type="text" onChange={handleChange} name="username" className="form-control" placeholder="Username" />
-        {errors.usernameError}
-    </div>
-    <div className="input-group mb-3">
-        <div className="input-group-prepend">
-            <span className="input-group-text"><i className="fa fa-lock"></i></span>
-        </div>
-        <input type="text" onChange={handleChange} name="password" className="form-control" placeholder="Password" />
-    </div>
-    <button type="button" className="btn btn-secondary btn-block">LOGIN</button>
-    <div className="message">
-        <div><input type="checkbox" /> Remember ME</div>
-        <div><a href="#">Forgot your password</a></div>
-    </div>
-    <div className="">
-        <div className='text-center'><Link className='txt-w' to="/register">Click here for create new account</Link></div>
-    </div>
-</form>
-        <div className="social">
-            <a href="#"><i className="fab fa-facebook"></i></a>
-            <a href="#"><i className="fab fa-twitter-square"></i></a>
-            <a href="#"><i className="fab fa-google"></i></a>
-        </div>
-    </div>
-</div>
+            <div className="form-box">
+                <div className="header-form">
+                    <h4 className="text-primary text-center"><i className="fa fa-user-circle" style={{ fontSize: "110px" }}></i></h4>
+                    <div className="image">
+                    </div>
+                </div>
+                <div className="body-form">
+                    <form method='post' onSubmit={LoginForm}>
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text"><i className="fa fa-user"></i></span>
+                            </div>
+                            <input type="text" onChange={handleChange} name="username" className="form-control" placeholder="Username" />
+                            {errors.usernameError}
+                        </div>
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text"><i className="fa fa-lock"></i></span>
+                            </div>
+                            <input type="text" onChange={handleChange} name="password" className="form-control" placeholder="Password" />
+                        </div>
+                        <button type="submit" className="btn btn-secondary btn-block">LOGIN</button>
+                        <div className="message">
+                            <div><input type="checkbox" /> Remember ME</div>
+                            <div><a href="#">Forgot your password</a></div>
+                        </div>
+                        <div className="">
+                            <div className='text-center'><Link className='txt-w' to="/register">Click here for create new account</Link></div>
+                        </div>
+                    </form>
+                    <div className="social">
+                        <a href="#"><i className="fab fa-facebook"></i></a>
+                        <a href="#"><i className="fab fa-twitter-square"></i></a>
+                        <a href="#"><i className="fab fa-google"></i></a>
+                    </div>
+                </div>
+            </div>
         </div>
 
     );
